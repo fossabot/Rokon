ae5383cadbfb2135265b7a4b8eddb39673fe6d36
Brycen G <brycengranville@outlook.com>
2024-09-02 00:21:05 -0400

    chore: remove vendor directory

--- vendor/github.com/jwijenbergh/puregotk/v4/glib/more_other.go
+++
@@ -1,8 +0,0 @@
-//go:build !windows && !darwin && !freebsd && (!linux || (!amd64 && !arm64))
-
-package glib
-
-// unrefCallback on unsupported on other platforms
-func unrefCallback(_ interface{}) error {
-	panic("glib.UnrefCallback is unsupported on this platform")
-}
--- vendor/github.com/jwijenbergh/puregotk/v4/glib/more_windows.go
+++
@@ -1,6 +0,0 @@
-package glib
-
-// unrefCallback on Windows is a NOOP.
-func unrefCallback(_ interface{}) error {
-	return nil
-}
