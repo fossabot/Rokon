e853e02c967ea895ab60705825e17fcb034e2304
Brycen G <brycengranville@outlook.com>
2024-09-06 08:50:45 -0400

    chore: rename rokon-alpine to rokon

--- virtual-desktop.sh
+++ virtual-desktop.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
-read Xenv < <(x11docker --xvfb --gpu --wayland --printenv rokon-alpine /app/rokon)
+# Build the docker image with the following command:
+# docker build -t rokon .
+# Using podman? Replace "docker" with "podman" in the command above.
+read Xenv < <(x11docker --xvfb --gpu --wayland --printenv rokon /app/rokon)
 echo $Xenv && export $Xenv
 # replace "start" with "start-desktop" to forward a desktop environment
 xpra start $DISPLAY --use-display \
