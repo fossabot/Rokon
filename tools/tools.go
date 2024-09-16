9979245696a0b5521a302824c54b0389cbbc298b
Brycen G <brycengranville@outlook.com>
2024-09-16 11:16:51 -0400

    chore: remove last remnants of goreleaser from tools

--- tools/tools.go
+++ tools/tools.go
@@ -10,6 +10,5 @@ package tools
 import (
 	_ "github.com/client9/misspell/cmd/misspell"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/goreleaser/goreleaser/v2"
 	_ "golang.org/x/vuln/cmd/govulncheck"
 )
