fossabot/Rokon

commit ba6d709aa2cb436e16e11d1a28e8c33cf4597a53

style(Makefile): add missing indentation [skip ci]

Makefile
@@ -181,18 +181,18 @@ tarball: ## build self contained Tarball that auto updates
 		echo "Sanity check succeeded."; \
 	fi
 
-ifeq ($(NOTB),1)
-	@echo "Finished making tarball directory. You have specified that a tarball shouldn't be created with NOTB=1"
-else
-	tar -czf $(TAR_NAME) $(TARBALLDIR)
-	@if command -v zsyncmake >/dev/null 2>&1; then \
-		zsyncmake $(TAR_NAME) -u "gh-releases-zsync|BrycensRanch|Rokon|latest|Rokon-$(shell uname)-*-$(shell uname -m).tar.gz.zsync"; \
-	else \
-		@echo "zsyncmake not found. Please install it to generate the zsync file."; \
-	fi
-	rm $(TARGET)
-	@echo "Tarball created: $(TAR_NAME)"
-endif
+	ifeq ($(NOTB),1)
+		@echo "Finished making tarball directory. You have specified that a tarball shouldn't be created with NOTB=1"
+	else
+		tar -czf $(TAR_NAME) $(TARBALLDIR)
+		@if command -v zsyncmake >/dev/null 2>&1; then \
+			zsyncmake $(TAR_NAME) -u "gh-releases-zsync|BrycensRanch|Rokon|latest|Rokon-$(shell uname)-*-$(shell uname -m).tar.gz.zsync"; \
+		else \
+			@echo "zsyncmake not found. Please install it to generate the zsync file."; \
+		fi
+		rm $(TARGET)
+		@echo "Tarball created: $(TAR_NAME)"
+	endif
 
 .PHONY: dev
 dev: ## go run -v .

commit 31de4c2635bd440f245a074485363ee881b480cf

build(tarball): remove erroneous indentation causing syntax error for conditional tarball
This syntax error was mistakenly introduced in commit ba6d709aa2cb436e16e11d1a28e8c33cf4597a53

and did not get checked by CI causing the commit 7b63845b6f8ff4412c8c48f1f36088c4d3a714db to fail inexpectedly.

Most importantly, it's because I didn't fully test my changes locally. Sorry

Makefile
@@ -181,9 +181,9 @@ tarball: ## build self contained Tarball that auto updates
 		echo "Sanity check succeeded."; \
 	fi
 
-	ifeq ($(NOTB),1)
+ifeq ($(NOTB),1)
 		@echo "Finished making tarball directory. You have specified that a tarball shouldn't be created with NOTB=1"
-	else
+else
 		tar -czf $(TAR_NAME) $(TARBALLDIR)
 		@if command -v zsyncmake >/dev/null 2>&1; then \
 			zsyncmake $(TAR_NAME) -u "gh-releases-zsync|BrycensRanch|Rokon|latest|Rokon-$(shell uname)-*-$(shell uname -m).tar.gz.zsync"; \
@@ -192,7 +192,7 @@ tarball: ## build self contained Tarball that auto updates
 		fi
 		rm $(TARGET)
 		@echo "Tarball created: $(TAR_NAME)"
-	endif
+endif
 
 .PHONY: dev
 dev: ## go run -v .